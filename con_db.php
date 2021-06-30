0489157ea301a69f70206d0fa7a87119c7ba1a37
skandinaviaCol <80217427+skandinaviaCol@users.noreply.github.com>
2021-06-30 11:35:39 -0500

    Update con_db.php

--- con_db.php
+++ con_db.php
@@ -1,6 +1,6 @@
 <?php
 
-$conexion = mysqli_connect("localhost","u684103415_wom","Skandinavia01*","u684103415_registradoswom");
+$conexion = mysqli_connect("localhost","u684103415_wom","3XY@ME0;zR","u684103415_registradoswom");
 
 if($conexion){
     //echo "Conexion Exitosa";
